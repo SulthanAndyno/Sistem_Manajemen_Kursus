3105cef885727ce0b2aa57bc08e70f249d96c9c6
SulthanHA <sulthandyno@gmail.com>
2025-11-30 05:33:22 +0700

    merubah type data double ke int

--- final/main.cpp
+++ final/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-/* ===================== STRUCTS ===================== */
+/* ==================== STRUCTS ===================== */
 struct Kursus {
     string idKursus;
     string namaKursus;
